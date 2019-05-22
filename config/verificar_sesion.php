e66d613a67c307ff819b83b22f7f3a3df7b7812b
YandryRo97 <yandry.yr99@gmail.com>
2019-05-21 19:10:18 -0500

    elimine archivos que no son necesarios

--- config/verificar_sesion.php
+++
@@ -1,6 +0,0 @@
-<?php
-    session_start();
-    if(!isset($_SESSION['isLogged'])|| $_SESSION['isLogged']===FALSE){
-        header("Location: /SistemaDeGestion/public/vista/login.html");
-    }
-?>
